91c63bf00d06dfcb12085490bc7f1d9357c8a61b
ankurbrdwj <ankurbrdwj@gmail.com>
2020-03-22 20:36:49 +0530

    algopractice

---
+++ tdd/src/test/java/BowlingTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+
+import org.junit.jupiter.api.DynamicTest;
+
+public class BowlingTest {
+
+    @DynamicTest
+    public void canCreateGame(){
+        Game game = new Game();
+    }
+
+
+}
